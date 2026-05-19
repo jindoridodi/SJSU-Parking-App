8881ac1c61079001e849bc33c0e05459c62d920d
Brian Ou <brianou7@gmail.com>
2026-05-19 15:59:21 -0700

    Initial Commit

---
+++ SJSU ParkingTests/SJSU_ParkingTests.swift
@@ -0,0 +1,17 @@
+//
+//  SJSU_ParkingTests.swift
+//  SJSU ParkingTests
+//
+//  Created by Brian Ou on 5/19/26.
+//
+
+import Testing
+@testable import SJSU_Parking
+
+struct SJSU_ParkingTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
