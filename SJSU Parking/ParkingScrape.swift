f4470874ae0a05ad66d73cf3d901592f3eca9d42
Brian Ou <brianou7@gmail.com>
2026-05-19 20:10:44 -0700

    ParkingScrape now returns an array of parking fullnesses

--- SJSU Parking/ParkingScrape.swift
+++ SJSU Parking/ParkingScrape.swift
@@ -10,30 +10,47 @@ import SwiftSoup
 
 final class ParkingScrape {
     // Function to get HTML of SJSU Parking Website
-    func getHTML() async {
+    func getHTML() async -> [Int]? {
         // SJSU Webpage
-        guard let url = URL(string: "https://sjsuparkingstatus.sjsu.edu/") else {
-            return
+        guard let url = URL(string: "https://sjsuparkingstatus.sjsu.edu/")
+        else {
+            return nil
         }
 
         do {
             // Get HTML
             let (data, _) = try await URLSession.shared.data(from: url)
-            guard let html = String(data: data, encoding: .utf8) else { return }
+            guard let html = String(data: data, encoding: .utf8) else {
+                return nil
+            }
 
             // Parse document
             let document = try SwiftSoup.parse(html)
 
             // Get Garage Parking
             let garageFullness = try document.select("span.garage__fullness")
-            
-            // Print to terminal
-            print("Garage Fullness:")
-            for garage in garageFullness{
-                print(try garage.text())
+
+            // Add to array
+            var garageFullnessArray = [Int]()
+            for garage in garageFullness {
+                let digits = try garage.text().filter { $0.isNumber }
+                if let number = Int(digits) {
+                    garageFullnessArray.append(number)
+                }
             }
+
+            // Print array
+            print("Garage Fullness Array:")
+            for garageFullness in garageFullnessArray {
+                print(garageFullness)
+            }
+
+            // Return Fullness Array
+            // [North Garage, West Garage, South Garage, South Campus Garage]
+            return garageFullnessArray
         } catch {
             print("Error: \(error)")
+            return nil
         }
     }
 }
